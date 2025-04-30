8d8c38ec6cb9dbe5d4fc53fd3ee7095d9c6e0f7e
abunaim1 <aabunnaim@gmail.com>
2025-04-30 20:23:17 +0600

    done with the models

--- api_with_drf/settings.py
+++ api_with_drf/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'api',
+
+    'django_extensions',
     
 ]
 
@@ -123,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'api.User'
