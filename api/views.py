971afc3119239a363e73bdabdeacff5e664661f7
abunaim1 <aabunnaim@gmail.com>
2025-07-15 14:00:22 +0600

    adding throttling by default

--- api/views.py
+++ api/views.py
@@ -1,12 +1,12 @@
 # from django.http import JsonResponse
+from rest_framework.decorators import api_view, action
+from django.shortcuts import get_object_or_404
 from django.db.models import Max
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
-# from django.views.decorators.vary import vary_on_headers
-from django.shortcuts import get_object_or_404
+from django.views.decorators.vary import vary_on_headers
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics, viewsets
-from rest_framework.decorators import api_view, action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
@@ -84,10 +84,10 @@ class OrderViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
 
 
-    # @method_decorator(cache_page(60 * 15, key_prefix='order_list'))
-    # @method_decorator(vary_on_headers("Authorization"))
-    # def list(self, request, *args, **kwargs):
-    #     return super().list(request, *args, **kwargs)
+    @method_decorator(cache_page(60 * 15, key_prefix='order_list'))
+    @method_decorator(vary_on_headers("Authorization"))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
